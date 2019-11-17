10bfd48dffa028ec6111755efc926b5e33fb819d
Lionel Jouin <lionel.jouin@outlook.com>
2019-11-17 16:44:42 +0100

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd ../dist
-zip -r -D ./dist/Alexa-Guess-Anything-API.zip *
-aws lambda update-function-code --function-name  Alexa-Guess-Anything-API --zip-file fileb://dist/Alexa-Guess-Anything-API.zip
+cd ./dist
+[ -e Alexa-Guess-Anything-API.zip ] && rm Alexa-Guess-Anything-API.zip
+zip -r -D Alexa-Guess-Anything-API.zip *
+aws lambda update-function-code --function-name  Alexa-Guess-Anything-API --zip-file fileb://Alexa-Guess-Anything-API.zip
